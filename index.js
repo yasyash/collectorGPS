ba949e403373f949b1715a3484192c51866c986c
Yasya <mail@ilit.ru>
2020-08-19 02:53:54 +0300

    Time framing injection...

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 
 
-var express = require ('express');
-var  bodyParser = require ('body-parser');
-var query = require ('./api/query');
+const express = require('express');
+const bodyParser = require('body-parser');
+const query = require('./api/query');
+const injector = require('./api/injector');
+
+const cron = require('node-cron');
 const https = require('https');
 const fs = require('fs');
+const superagent = require('superagent');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use ('/query', query);
+app.use('/query', query);
 
 const options = {
-    key: fs.readFileSync('./keys/chel_key.key'),
-    cert: fs.readFileSync('./keys/chel_cert.crt')
-  };
+  key: fs.readFileSync('./keys/chel_key.key'),
+  cert: fs.readFileSync('./keys/chel_cert.crt')
+};
 
 
+https.createServer(options, app).listen(8383, () => {
 
-https.createServer(options, app).listen(8484, () => {
-    console.log('Server SSL is started on 8484 port...');
+  console.log('Server SSL is started on 8383 port...');
+  cron.schedule("* * * * *", () => {
+    console.log("injection");
+    injector();
+  });
+
 });
 
 
